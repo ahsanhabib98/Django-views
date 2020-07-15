cbc6b3d9092be40256a6e4ae5007ea577f8b037a
Ahsan Habib <ahredoan@gmail.com>
2020-07-15 18:33:11 +0600

    dockerize django app and added mysql db

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# Django-views
+# Django + Mysql Docker
+
+Run the following commands to get started:
+
+````
+docker-compose build
+docker-compose up
+````
--- blog/models.py
+++ blog/models.py
@@ -8,21 +8,21 @@ from datetime import timedelta, datetime, date
 from django.db import models
 from django.db.models.signals import pre_save, post_save
 from django.utils.encoding import smart_text
-from django.utils import timezone 
+from django.utils import timezone
 from django.utils.text import slugify
 from django.utils.timesince import timesince
-
 
 # Create your models here.
 
 
-#from .validators import validate_author_email, validate_justin
+# from .validators import validate_author_email, validate_justin
 
 PUBLISH_CHOICES = [
-        ('draft', 'Draft'),
-        ('publish', 'Publish'),
-        ('private', 'Private'),
-    ]
+    ('draft', 'Draft'),
+    ('publish', 'Publish'),
+    ('private', 'Private'),
+]
+
 
 class PostModelQuerySet(models.query.QuerySet):
     def active(self):
@@ -37,43 +37,45 @@ class PostModelManager(models.Manager):
         return PostModelQuerySet(self.model, using=self._db)
 
     def all(self, *args, **kwargs):
-        #qs = super(PostModelManager, self).all(*args, **kwargs).active() #.filter(active=True)
-        #print(qs)
+        # qs = super(PostModelManager, self).all(*args, **kwargs).active() #.filter(active=True)
+        # print(qs)
         qs = self.get_queryset().active()
         return qs
 
     def get_timeframe(self, date1, date2):
-        #assume datetime objects
+        # assume datetime objects
         qs = self.get_queryset()
         qs_time_1 = qs.filter(publish_date__gte=date1)
-        qs_time_2 = qs_time_1.filter(publish_date__lt=date2) # Q Lookups
-        #final_qs = (qs_time_1 | qs_time_2).distinct()
+        qs_time_2 = qs_time_1.filter(publish_date__lt=date2)  # Q Lookups
+        # final_qs = (qs_time_1 | qs_time_2).distinct()
         return qs_time_2
 
+
 class PostModel(models.Model):
-    id              = models.BigAutoField(primary_key=True)
-    active          = models.BooleanField(default=True) #empty in the database
-    title           = models.CharField(
-                            max_length=240, 
-                            verbose_name='Post title', 
-                            unique=True,
-                            error_messages={
-                                "unique": "This title is not unique, please try again.",
-                                "blank": "This field is not full, please try again."
-                            },
-                            help_text='Must be a unique title.')
-    slug            = models.SlugField(null=True, blank=True)
-    content         = models.TextField(null=True, blank=True)
-    publish         = models.CharField(max_length=120, choices=PUBLISH_CHOICES, default='draft')
-    view_count      = models.IntegerField(default=0)
-    publish_date    = models.DateField(auto_now=False, auto_now_add=False, default=timezone.now)
-    author_email    = models.EmailField(max_length=240, null=True, blank=True)
-    updated         = models.DateTimeField(auto_now=True)
-    timestamp       = models.DateTimeField(auto_now_add=True)
+    id = models.BigAutoField(primary_key=True)
+    active = models.BooleanField(default=True)  # empty in the database
+    title = models.CharField(
+        max_length=240,
+        verbose_name='Post title',
+        unique=True,
+        error_messages={
+            "unique": "This title is not unique, please try again.",
+            "blank": "This field is not full, please try again."
+        },
+        help_text='Must be a unique title.')
+    slug = models.SlugField(null=True, blank=True)
+    content = models.TextField(null=True, blank=True)
+    publish = models.CharField(max_length=120, choices=PUBLISH_CHOICES, default='draft')
+    view_count = models.IntegerField(default=0)
+    publish_date = models.DateField(auto_now=False, auto_now_add=False, default=timezone.now)
+    author_email = models.EmailField(max_length=240, null=True, blank=True)
+    updated = models.DateTimeField(auto_now=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
 
     objects = PostModelManager()
     other = PostModelManager()
-    #save = PostModelManager()
+
+    # save = PostModelManager()
 
     def save(self, *args, **kwargs):
         # if not self.slug and self.title:
@@ -83,12 +85,13 @@ class PostModel(models.Model):
     class Meta:
         verbose_name = 'Post'
         verbose_name_plural = 'Posts'
-        #unique_together = [('title', 'slug')]
+        ordering = ('title',)
+        # unique_together = [('title', 'slug')]
 
-    def __unicode__(self): #python 2
-        return smart_text(self.title) #self.title
+    def __unicode__(self):  # python 2
+        return smart_text(self.title)  # self.title
 
-    def __str__(self): #python 3
+    def __str__(self):  # python 3
         return smart_text(self.title)
 
     @property
@@ -96,27 +99,26 @@ class PostModel(models.Model):
         if self.publish == 'publish':
             now = datetime.now()
             publish_time = datetime.combine(
-                                self.publish_date,
-                                datetime.now().min.time()
-                        )
+                self.publish_date,
+                datetime.now().min.time()
+            )
             try:
                 difference = now - publish_time
             except:
                 return "Unknown"
             if difference <= timedelta(minutes=1):
                 return 'just now'
-            return '{time} ago'.format(time= timesince(publish_time).split(', ')[0])
+            return '{time} ago'.format(time=timesince(publish_time).split(', ')[0])
         return "Not published"
-
 
 
 def blog_post_model_pre_save_receiver(sender, instance, *args, **kwargs):
     print("before save")
     if not instance.slug and instance.title:
-        instance.slug = slugify(instance.title) 
+        instance.slug = slugify(instance.title)
+
 
 pre_save.connect(blog_post_model_pre_save_receiver, sender=PostModel)
-
 
 
 def blog_post_model_post_save_receiver(sender, instance, created, *args, **kwargs):
@@ -127,13 +129,8 @@ def blog_post_model_post_save_receiver(sender, instance, created, *args, **kwarg
             instance.slug = slugify(instance.title)
             instance.save()
 
+
 post_save.connect(blog_post_model_post_save_receiver, sender=PostModel)
-
-
-
-
-
-
 
 '''
 python manage.py makemigrations #every time you change models.py
